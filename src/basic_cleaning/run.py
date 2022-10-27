37794d2495b6cbcaebfdb81ec57ba5b5ce6e0d32
Gabriel <gab.neiva@hotmail.com>
2022-10-26 21:03:45 -0300

    add longitude cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -23,11 +23,12 @@ def go(args):
 
     # Cleaning
     logger.info("Dropping Outliers")
-    min_price = args.min_price
-    max_price = args.max_price
-    idx = df['price'].between(min_price, max_price)
+    idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
-
+    
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+    
     # Saving
     logger.info("Uploading output artifact to W&B")
     df.to_csv(args.output_artifact, index=False)
